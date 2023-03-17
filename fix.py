e404f4eb3aaf4f8b1939ddba52995cee2dacc1cb
Suraj <surajprajapati8286@gmail.com>
2023-03-17 14:42:43 +0530

    final submit

---
+++ fix.py
@@ -0,0 +1,28 @@
+from cx_Freeze import setup, Executable
+import sys
+import os
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR,'tcl','tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR,'tcl','tcl8.6')
+
+base = None
+
+if sys.platform == 'win32':
+    base = None
+
+executables = [Executable("attendance.py",base=base)]
+packages = ["inda","os","sys","sx_Freeze","tkinter","cv2","setup","numpy","PIL","pandas","datetime","time"]
+
+options = {
+    'build_exe':{
+        'packages':packages,
+    },
+}
+
+setup(
+    name = "ToolBax",
+    options = options,
+    version = "0.0.1",
+    description = 'Vision ToolBox',
+    executables = executables
+)
